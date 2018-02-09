a19e804b1769e513cceb4d6af86b0fd519d07120
Ben Nham <nham@fb.com>
2018-02-09 11:16:37 -0800

    add formula for breakpad minidump tools

---
+++ breakpad_minidump.rb
@@ -0,0 +1,22 @@
+class BreakpadMinidump < Formula
+  desc "breakpad_minidump is a set of tools that can be used to inspect Google Breakpad minidumps"
+  homepage "https://chromium.googlesource.com/breakpad/breakpad/"
+  head "https://chromium.googlesource.com/breakpad/breakpad", :branch => "chrome_64"
+
+  depends_on :xcode
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+
+    # Install manually since we only want to install the binaries to
+    # #{prefix}/bin. We don't want the headers or libraries installed.
+    bin.install "src/processor/minidump_dump"
+    bin.install "src/processor/minidump_stackwalk"
+    bin.install "src/processor/microdump_stackwalk"
+  end
+
+  test do
+    system "#{bin}/minidump_dump", "-h"
+  end
+end
